276e28c347bcadb21e464cd52d0a0e63a5c44c49
Rafael <rafael.19.rosendo@gmail.com>
2024-12-09 13:39:58 -0300

    desenho quase pronto

--- ex1.py
+++ ex1.py
@@ -1,28 +1,35 @@
 import turtle
 from collections import deque
-
+import argparse
+import json
+Y = (0, 255, 0)
 class LSystem:
-    def __init__(self, axiom, rules, angle=60):
+    def __init__(self, axiom, rules, angle, distance, largura):
         self.axiom = axiom
         self.rules = rules
         self.angle = angle
         self.result = ""
+        self.distance = distance
+        self.largura = largura
 
     def generate(self, iterations):
         self.result = self.axiom
         for _ in range(iterations):
             self.result = "".join(self.rules.get(ch, ch) for ch in self.result)
 
-    def draw(self):
+    def draw(self, distance, largura):
         stack = deque()
         turtle.speed(0)
-        turtle.width(5)
+        turtle.width(largura)
         for cmd in self.result:
             if cmd == "F":
-                turtle.forward(20)
+                #turtle.pencolor(0, 255, 0)
+                turtle.color("black")
+                turtle.forward(distance)
+
             elif cmd == "f":
                 turtle.penup()
-                turtle.forward(20)
+                turtle.forward(distance)
                 turtle.pendown()
             elif cmd == "+":
                 turtle.right(self.angle)
@@ -41,9 +48,33 @@ class LSystem:
             elif cmd.startswith("S"): #tamanho do galho
                 size = int(cmd[1:])
                 turtle.width(size)
-            elif cmd.startswith("C"): #caso tenha cores diferentes
-                color = tuple(map(int, cmd[1:].split(",")))
-                turtle.pencolor(color)
+            elif cmd.startswith("G"): #cor
+                turtle.color("green")
+                turtle.forward(distance)
+            elif cmd.startswith("B"): #cor
+                turtle.color("blue")
+                turtle.forward(distance)
+            elif cmd.startswith("R"): #cor
+                turtle.color("red")
+                turtle.forward(distance)
+            elif cmd.startswith("Y"): #cor
+                turtle.color("yellow")
+                turtle.forward(distance)
+            elif cmd.startswith("O"): #cor
+                turtle.color("orange")
+                turtle.forward(distance)
+
+
+
+'''
+'''
+
+def parse_rules(rules_str):
+    rules = {}
+    for rule in rules_str.split(","):
+        key, value = rule.split(":")
+        rules[key] = value
+    return rules
 
 if __name__ == "__main__":
 
@@ -51,10 +82,30 @@ if __name__ == "__main__":
     rules = {
         "F": "F[+F][-F]"
     }
-    angle = 60
+    #angle = 60
 
-    lsys = LSystem(axiom, rules, angle)
-    lsys.generate(6)  #aqui é o N
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--d", type=int, default=10)
+    parser.add_argument("--a", type=int, default=60)
+    parser.add_argument("--i", type=int, default=6)
+    #parser.add_argument("--axiom", type=str, default="F")
+    parser.add_argument("--l" , type=int, default=1)
+    parser.add_argument("--axim", type=str, default="F")
+    #parser.add_argument("--axiom", type=str, default="F")
+    parser.add_argument("--rules", type=str, default="F:F[+F][-F]")
+    args = parser.parse_args()
+
+    angle = args.a
+    distance = args.d
+    #axiom = args.axiom
+    iter = args.i
+    largura = args.l
+    #rules = args.rules
+    axiom = args.axim
+    rules = parse_rules(args.rules)
+
+    lsys = LSystem(axiom, rules, angle, distance, largura)
+    lsys.generate(iter)  #aqui é o N
     print(lsys.result)
-    lsys.draw()
+    lsys.draw(distance, largura)
     turtle.done()
